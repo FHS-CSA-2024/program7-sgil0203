403b5f2d157eac914b82b03066ac35bac0298382
sgil0203 <surbaanigill@gmail.com>
2024-09-23 21:50:58 -0700

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,9 +1,32 @@
-//import stuff here
-
+import java.util.Scanner; 
+public class program7 {
+    public static void main (String[]args) {
+        int newStanley = 0; 
+        Scanner myScanner = new Scanner(System.in); 
+        System.out.println("Enter Schrute-Bucks: "); 
+        int schrute = myScanner.nextInt(); 
+        System.out.println("Enter Klevins: "); 
+        int klevin = myScanner.nextInt(); 
+        System.out.println("Enter Stanley-Nickels: "); 
+        int oldStanley = myScanner.nextInt(); 
+        final double stanleyInSchrute = 240.0;
+        final double klevinsInSchrute = 20.0; 
+        final double stanleyInKlevin = 12.0; 
+        double answer1 = ((stanleyInKlevin * klevin + oldStanley)/stanleyInSchrute)+schrute;
+        double answerFinal = ((int)((answer1 + 0.005) * 100)) / 100.0;
+        System.out.println("Decimal schrute-bucks: $" + answerFinal);
+}
 //Your code here
 
 //Paste console output below:
 /*
-
+Enter Schrute-Bucks: 
+7
+Enter Klevins: 
+17
+Enter Stanley-Nickels: 
+9
+Decimal schrute-bucks: $7.89
 
 */
+
